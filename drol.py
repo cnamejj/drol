1b92c373084414e98f2cec021f6349ed286c9867
Bradley Sadowsky <bradley.sadowsky@hotmail.com>
2016-11-23 22:03:41 -0500

    DROL Version 1.1

--- drol.py
+++ drol.py
@@ -14,156 +14,178 @@ def compile(drolstr, outfile): # Compile DROL code into C code
     init = '#include <stdio.h>\n#include <stdlib.h>\nvoid main(void) {\n\tint regone = 0;\n\tint regtwo = 0;\n'
     # End of C file
     end = '}\n'
-    # Increment register one by 1
-    i = '\tregone = regone + 1;\n'
-    # Increment register two by 1
-    k = '\tregtwo = regtwo + 1;\n'
-    # Decrement register one by 1
-    d = '\tregone = regone - 1;\n'
-    # Decrement register two by 1
-    e = '\tregwto = regtwo = 1;\n'
-    # Display register one to the screen
-    o = '\tprintf("%d", regone);\n'
-    # Display register two to the screen
-    p = '\tprintf("%d", regtwo);\n'
-    # Display register one to the screen IF REGISTER ONE IS ASCII (0-255)
-    a = '\tif (regone >= 0 && regone <= 255) {\n\t\tprintf("%c", regone);\n\t}\n'
-    # Display register two to the screen IF REGISTER ONE IS ASCII (0-255)
-    b = '\tif (regtwo >= 0 && regtwo <= 255) {\n\t\tprintf("%c", regtwo);\n\t}\n'
-    # Display newline
-    newline = '\tprintf("\\n");\n'
-    # Set register one to user inputted number
-    n = '\tscanf("%d", &regone);\n'
-    # Set register two to user inputted number
-    q = '\tscanf("%d", &regtwo);\n'
-    # Set register one to zero
-    z = '\tregone = 0;\n'
-    # Set register two to zero
-    y = '\tregtwo = 0;\n'
-    # Set both registers to the contents of register one
-    one = '\tregtwo = regone;\n'
-    # Set both registers to the contents of register two
-    two = '\tregone = regtwo;\n'
-    # Set register one to the sum of the registers
-    s = '\tregone = regone + regtwo;\n'
-    # Set register two to the sum of the registers
-    u = '\tregtwo = regone + regtwo;\n'
-    # Set register one to the difference of the registers
-    f = '\tregone = regone - regtwo;\n'
-    # Set register two to the difference of the registers
-    g = '\tregtwo = regone - regtwo;\n'
-    # Set register one to the product of the registers
-    m = '\tregone = regone * regtwo;\n'
-    # Set register two to the product of the registers
-    r = '\tregtwo = regone * regtwo;\n'
-    # Set register one to register one squared
-    e = '\tregone = regone * regone;\n'
-    # Set register two to register two squared
-    h = '\tregtwo = regtwo * regtwo;\n'
-    # Tests if the registers are equal; If not equal, the program exits
-    j = '\tif (regone != regtwo) {\n\t\texit(0);\n\t}\n'
-    # Swaps register one and register two
-    swap = '\tint tempone = regone;\n\tint temptwo = regtwo;\n\tregone = temptwo;\n\tregtwo = tempone;\n'
-    # Left shift register one by one
-    lshftone = '\tregone = regone << 1;\n'
-    # Left shift register two by one
-    lshfttwo = '\tregtwo = regtwo << 1;\n'
-    # Right shift register one by one
-    rshftone = '\tregone = regone >> 1;\n'
-    # Right shift register two by one
-    rshfttwo = '\tregtwo = regtwo >> 1;\n'
-    # Flip bits in register one
-    flipone = '\tregone = ~regone;\n'
-    # Flip bits in register two
-    fliptwo = '\tregtwo = ~regtwo;\n'
-    # XOR register one to register two
-    xorone = '\tregone = regone ^ regtwo;\n'
-    # XOR register two to register one
-    xortwo = '\tregtwo = regone ^ regone;\n'
-    # OR register one to register two
-    orone = '\tregone = regone | regtwo;\n'
-    # OR register two to register one
-    ortwo = '\tregtwo = regone | regtwo;\n'
-    # AND register one to register two
-    andone = '\tregone = regone & regtwo;\n'
-    # AND register two to register one
-    andtwo = '\tregtwo = regone & regtwo;\n'
-    outfile.write(init)
-    for char in drolstr:
-        if char == "i":
-            outfile.write(i)
+    def lookup(char):
+        # Exit the program successfully
+        x = '\texit(0);\n'
+        # Increment register one by 1
+        i = '\tregone = regone + 1;\n'
+        # Increment register two by 1
+        k = '\tregtwo = regtwo + 1;\n'
+        # Decrement register one by 1
+        d = '\tregone = regone - 1;\n'
+        # Decrement register two by 1
+        e = '\tregwto = regtwo = 1;\n'
+        # Display register one to the screen
+        o = '\tprintf("%d", regone);\n'
+        # Display register two to the screen
+        p = '\tprintf("%d", regtwo);\n'
+        # Display register one to the screen IF REGISTER ONE IS ASCII (0-255)
+        a = '\tif (regone >= 0 && regone <= 255) {\n\t\tprintf("%c", regone);\n\t}\n'
+        # Display register two to the screen IF REGISTER ONE IS ASCII (0-255)
+        b = '\tif (regtwo >= 0 && regtwo <= 255) {\n\t\tprintf("%c", regtwo);\n\t}\n'
+        # Display newline
+        newline = '\tprintf("\\n");\n'
+        # Set register one to user inputted number
+        n = '\tscanf("%d", &regone);\n'
+        # Set register two to user inputted number
+        q = '\tscanf("%d", &regtwo);\n'
+        # Set register one to zero
+        z = '\tregone = 0;\n'
+        # Set register two to zero
+        y = '\tregtwo = 0;\n'
+        # Set both registers to the contents of register one
+        one = '\tregtwo = regone;\n'
+        # Set both registers to the contents of register two
+        two = '\tregone = regtwo;\n'
+        # Set register one to the sum of the registers
+        s = '\tregone = regone + regtwo;\n'
+        # Set register two to the sum of the registers
+        u = '\tregtwo = regone + regtwo;\n'
+        # Set register one to the difference of the registers
+        f = '\tregone = regone - regtwo;\n'
+        # Set register two to the difference of the registers
+        g = '\tregtwo = regone - regtwo;\n'
+        # Set register one to the product of the registers
+        m = '\tregone = regone * regtwo;\n'
+        # Set register two to the product of the registers
+        r = '\tregtwo = regone * regtwo;\n'
+        # Set register one to register one squared
+        e = '\tregone = regone * regone;\n'
+        # Set register two to register two squared
+        h = '\tregtwo = regtwo * regtwo;\n'
+        # Swaps register one and register two
+        swap = '\tint tempone = regone;\n\tint temptwo = regtwo;\n\tregone = temptwo;\n\tregtwo = tempone;\n'
+        # Left shift register one by one
+        lshftone = '\tregone = regone << 1;\n'
+        # Left shift register two by one
+        lshfttwo = '\tregtwo = regtwo << 1;\n'
+        # Right shift register one by one
+        rshftone = '\tregone = regone >> 1;\n'
+        # Right shift register two by one
+        rshfttwo = '\tregtwo = regtwo >> 1;\n'
+        # Flip bits in register one
+        flipone = '\tregone = ~regone;\n'
+        # Flip bits in register two
+        fliptwo = '\tregtwo = ~regtwo;\n'
+        # XOR register one to register two
+        xorone = '\tregone = regone ^ regtwo;\n'
+        # XOR register two to register one
+        xortwo = '\tregtwo = regone ^ regone;\n'
+        # OR register one to register two
+        orone = '\tregone = regone | regtwo;\n'
+        # OR register two to register one
+        ortwo = '\tregtwo = regone | regtwo;\n'
+        # AND register one to register two
+        andone = '\tregone = regone & regtwo;\n'
+        # AND register two to register one
+        andtwo = '\tregtwo = regone & regtwo;\n'
+        
+        if char == "x":
+            return x
+        elif char == "0":
+            return ""
+        elif char == "i":
+            return i
         elif char == "k":
-            outfile.write(k)
+            return k
         elif char == "d":
-            outfile.write(d)
+            return d
         elif char == "e":
-            outfile.write(e)
+            return e
         elif char == "o":
-            outfile.write(o)
+            return o
         elif char == "p":
-            outfile.write(p)
+            return p
         elif char == "a":
-            outfile.write(a)
+            return a
         elif char == "b":
-            outfile.write(b)
+            return b
         elif char == "!":
-            outfile.write(newline)
+            return newline
         elif char == "n":
-            outfile.write(n)
+            return n
         elif char == "q":
-            outfile.write(q)
+            return q
         elif char == "z":
-            outfile.write(z)
+            return z
         elif char == "y":
-            outfile.write(y)
+            return y
         elif char == "1":
-            outfile.write(one)
+            return one
         elif char == "2":
-            outfile.write(two)
+            return two
         elif char == "s":
-            outfile.write(s)
+            return s
         elif char == "u":
-            outfile.write(u)
+            return u
         elif char == "f":
-            outfile.write(f)
+            return f
         elif char == "g":
-            outfile.write(g)
+            return g
         elif char == "m":
-            outfile.write(m)
+            return m
         elif char == "r":
-            outfile.write(r)
+            return r
         elif char == "e":
-            outfile.write(e)
+            return e
         elif char == "h":
-            outfile.write(h)
-        elif char == "j":
-            outfile.write(j)
+            return h
         elif char == "*":
-            outfile.write(swap)
+            return swap
         elif char == "<":
-            outfile.write(lshftone)
+            return lshftone
         elif char == ",":
-            outfile.write(lshfttwo)
+            return lshfttwo
         elif char == ">":
-            outfile.write(rshftone)
+            return rshftone
         elif char == ".":
-            outfile.write(rshfttwo)
+            return rshfttwo
         elif char == "~":
-            outfile.write(flipone)
+            return flipone
         elif char == "`":
-            outfile.write(fliptwo)
+            return fliptwo
         elif char == "^":
-            outfile.write(xorone)
+            return xorone
         elif char == "6":
-            outfile.write(xortwo)
+            return xortwo
         elif char == "|":
-            outfile.write(orone)
+            return orone
         elif char == "\\":
-            outfile.write(ortwo)
+            return ortwo
         elif char == "&":
-            outfile.write(andone)
+            return andone
         elif char == "7":
-            outfile.write(andtwo)
+            return andtwo
+        else:
+            return ""
+    
+    outfile.write(init)
+    drolitr = iter(drolstr.lower())
+    location = 0
+    for char in drolitr:
+        if char == "j": # If registers are equal execute next command, else skip
+            code = '\tif (regone == regtwo) {\n\t\t' + lookup(drolstr[location + 1]) + '\n\t}\n'
+            outfile.write(code)
+            location = location + 2
+            drolitr.next()
+        elif char == "w": # While the registers are not equal, the next command is executed
+            code = '\twhile (regone != regtwo) {\n\t\t' + lookup(drolstr[location + 1]) + '\n\t}\n'
+            outfile.write(code)
+            location = location + 2
+            drolitr.next()
+        else:
+            outfile.write(lookup(char))
+            location = location + 1
     outfile.write(end)
 
 def main(argnum = 1):
