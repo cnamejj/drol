43da2c8460b0766960a79f8fddc419f8fe1b5680
Bradley Sadowsky <bradley.sadowsky@hotmail.com>
2016-11-24 20:18:41 -0500

    DROL Version 1.5

--- drol.py
+++ drol.py
@@ -174,10 +174,10 @@ def compile(drolstr, outfile): # Compile DROL code into C code
     location = 0
     for char in drolitr:
         if char == "j": # If registers are equal execute next command, else skip
-            code = '\tif (regone == regtwo) {\n\t\t' + lookup(drolstr[location + 1]) + lookup(drolstr[location + 2]) + '\n\t} else {\n\t\t' + lookup(drolstr[location + 3]) + lookup(drolstr[location + 4]) + '\n\t}\n'
-            outfile.write(code)
-            location = location + 5
             try:
+                code = '\tif (regone == regtwo) {\n\t\t' + lookup(drolstr[location + 1]) + lookup(drolstr[location + 2]) + '\n\t} else {\n\t\t' + lookup(drolstr[location + 3]) + lookup(drolstr[location + 4]) + '\n\t}\n'
+                outfile.write(code)
+                location = location + 5
                 drolitr.next()
                 drolitr.next()
                 drolitr.next()
@@ -187,10 +187,10 @@ def compile(drolstr, outfile): # Compile DROL code into C code
             except IndexError:
                 pass
         elif char == "w": # While the registers are not equal, the next command is executed
-            code = '\twhile (regone != regtwo) {\n\t\t' + lookup(drolstr[location + 1]) + lookup(drolstr[location + 2]) + lookup(drolstr[location + 3]) + '\n\t}\n'
-            outfile.write(code)
-            location = location + 4
             try:
+                code = '\twhile (regone != regtwo) {\n\t\t' + lookup(drolstr[location + 1]) + lookup(drolstr[location + 2]) + lookup(drolstr[location + 3]) + '\n\t}\n'
+                outfile.write(code)
+                location = location + 4
                 drolitr.next()
                 drolitr.next()
                 drolitr.next()
